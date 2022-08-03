2033ad9082b113a13ee0e669b76650564effef0d
maxwellyoung <maxtheyoung@gmail.com>
2022-08-03 21:45:28 +1200

    deploy2

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -1,37 +1,40 @@
 const path = require('path')
 
 module.exports = {
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: path.join(__dirname, 'dev.sqlite3')
-    },
-    useNullAsDefault: true
-  },
+	development: {
+		client: 'sqlite3',
+		connection: {
+			filename: path.join(__dirname, 'dev.sqlite3'),
+		},
+		useNullAsDefault: true,
+	},
 
-  test: {
-    client: 'sqlite3',
-    connection: {
-      filename: ':memory:'
-    },
-    migrations: {
-      directory: path.join(__dirname, 'migrations')
-    },
-    seeds: {
-      directory: path.join(__dirname, 'seeds')
-    },
-    useNullAsDefault: true
-  },
+	test: {
+		client: 'sqlite3',
+		connection: {
+			filename: ':memory:',
+		},
+		migrations: {
+			directory: path.join(__dirname, 'migrations'),
+		},
+		seeds: {
+			directory: path.join(__dirname, 'seeds'),
+		},
+		useNullAsDefault: true,
+	},
 
-  production: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+	production: {
+		client: 'postgresql',
+		connection: {
+			connectionString: process.env.DATABASE_URL,
+			ssl: { rejectUnauthorized: false },
+		},
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		migrations: {
+			tableName: 'knex_migrations',
+		},
+	},
 }
